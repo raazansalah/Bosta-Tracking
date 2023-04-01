12a5cd67d4f6edf37c19b99880c73dd52e5819a9
raazansalah <razansalah022@gmail.com>
2023-04-01 17:27:43 +0200

    ADDED: addres info section

--- src/components/AddressInfo/index.tsx
+++ src/components/AddressInfo/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function AddressInfo() {
   return (
-    <div className=' rounded-lg border-[1px] border-[#EEEEEE] w-full p-6 mt-4'>
-        <p></p>
+    <div className=' rounded-lg bg-[#FBFBFB] border-[1px] border-[#EEEEEE] w-full p-6 mt-4'>
+        <p className='text-[#4B566F]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae nisi ea placeat omnis libero ullam ipsum. Ut praesentium sed porro assumenda architecto expedita, minima sint rerum maxime velit. Cupiditate, alias.</p>
     </div>
   )
 }
