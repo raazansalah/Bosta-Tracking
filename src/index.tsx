73584839d78ef2669ceb106a7836f3e03a5fc96d
raazansalah <razansalah022@gmail.com>
2023-04-01 13:26:58 +0200

    ADDED: routing structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App';
 import './index.css';
+import ShipmentTracking from './pages/ShipmentTracking';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path:'/tracking-shipments/:trackingNumber',
+    element: <ShipmentTracking />,
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
