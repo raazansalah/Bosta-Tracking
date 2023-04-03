fb4510d4efd7cfa9fa5aa2746997459ed495457b
raazansalah <razansalah022@gmail.com>
2023-04-03 16:41:35 +0200

    feat: switch translations

--- src/components/ShipmentInfo/index.tsx
+++ src/components/ShipmentInfo/index.tsx
@@ -10,14 +10,14 @@ export default function ShipmentInfo() {
   const {status,trackingNumber,latestUpdate,merchantName,promisedDate,activeColor} = useSelector((state: any) => state.shipment);
   const { t } = useTranslation();
   return (
-    <div className=' divide-y border-[1px] border-[#EEEEEE] rounded-lg w-full '>
+    <div className=' divide-y border-[1px] border-[#EEEEEE] rounded-lg w-full  '>
      <div className=' grid grid-cols-1 gap-4  p-6 lg:grid-cols-4 md:grid-cols-4 '>
      <InfoElement title={`${t('SHIPMENT_NUMBER')} ${trackingNumber}`} value={status} activeColor={activeColor}/>
       <InfoElement title={t('LATEST')} value={latestUpdate} />
       <InfoElement title={t('MERCHANT_NAME')} value={merchantName} />
       <InfoElement title={t('DATE')} value={promisedDate} />
      </div>
-     <div className='p-6 lg:block md:block hidden'>
+     <div className='p-6 lg:block md:block hidden w-full'>
      <TrackingStepper status={status}/>
      </div>
     </div>
--- src/pages/ShipmentTracking/index.tsx
+++ src/pages/ShipmentTracking/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import AddressInfo from '../../components/AddressInfo'
 import ReportProblem from '../../components/ReportProblem'
@@ -6,8 +7,27 @@ import ShipmentInfo from '../../components/ShipmentInfo'
 
 export default function ShipmentTracking() {
   const {t} = useTranslation()
+  const { i18n } = useTranslation();
+  const [direction, setDirection] = useState('ltr')
+
+  function changeLanguage(e:any) {
+    i18n.changeLanguage(e.target.value);
+    if(e.target.value === 'ar'){
+      setDirection('rtl')
+    }else{
+      setDirection('ltr')
+    }
+  }
+
   return (
-    <div className='flex justify-center flex-col items-center '>
+    <div className='flex justify-center flex-col items-center ' dir={direction}>
+      <div>
+      <select onChange={changeLanguage}>
+        <option value="ar">AR</option>
+        <option value="en">EN</option>
+      </select>
+
+      </div>
         <ShipmentInfo />
         <div className='grid grid-flow-row-dense grid-cols-3 gap-12 w-full mt-8'>
             <div className='col-span-3  lg:col-span-2'>
